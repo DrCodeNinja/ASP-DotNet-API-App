8bbced7b071b6cc0123fba093c5d16076cc9846c
Dasun Dasanayaka <dasanayakabandara81@gmail.com>
2024-09-28 10:58:44 +0530

    Add project files.

---
+++ TestApp/Controllers/TasksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult TasksLists()
+        {
+            var tasks = new string[] { "Task 1"};
+            return Ok(tasks);
+        }
+    }
+}
